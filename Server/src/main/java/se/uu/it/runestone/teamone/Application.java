2fa3134f62829ce296790a3182b9877ac448ba1b
Andreas Lelli <andreas@lelli.se>
2015-05-14 17:37:48 +0200

    Reverted some changes that did not work

--- Server/src/main/java/se/uu/it/runestone/teamone/Application.java
+++ Server/src/main/java/se/uu/it/runestone/teamone/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.integration.annotation.IntegrationComponentScan;
-//Test
-import se.uu.it.runestone.teamone.map.Room;
 
 
 /**
